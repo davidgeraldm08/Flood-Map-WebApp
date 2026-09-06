4b62d08b713556e23d97563821ba92f62c80d7cb
Gerald <[davidgeraldm08@gmail.com]>
2026-09-06 13:50:11 +0800

    Prepare for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,16 @@
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 
-const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "flood_map",
-    password: "123456",
-    port: 5432
-
-
-});
+const pool = process.env.DATABASE_URL
+    ? new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+    })
+    : new Pool({
+        user: "postgres",
+        host: "localhost",
+        database: "flood_map",
+        password: "123456",
+        port: 5432,
+    });
 
 module.exports = pool;
